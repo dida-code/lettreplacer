ccc07bff60a76e19da7d36b20b3f43cef3eb3cc7
Dimitrije Kocic <142322539+dida-code@users.noreply.github.com>
2024-05-15 22:52:53 +0200

    Update window.py

--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright 2023 dida-code
+# Copyright 2024 dida-code
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
